82cb7dc1cdc1ed62ea2ba508b8a3f041114b7431
Aida0707 <aida071194@gmail.com>
2020-05-02 15:07:27 +0000

    added task3

--- task3/backup.tf
+++ task3/backup.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "dev" {
   key_name      =     "${aws_key_pair.us-east-1-key.key_name}"
   vpc_security_group_ids = ["sg-0c702dc82f44657b7"]
   #user_data = "${file("user_data.sh")}"
-  subnet_id = "${aws_subnet.public1.id}"
+  subnet_id = "${aws_subnet.private1.id}"
  tags = "${var.tags}"
   
 }
